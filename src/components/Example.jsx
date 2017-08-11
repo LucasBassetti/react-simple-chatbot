70bc14c7b4d0e05661ca8320fdada12da78ae013
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2017-08-11 20:09:19 -0300

    fix src example

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -13,49 +13,17 @@ const otherFontTheme = {
   userFontColor: '#4a4a4a',
 };
 
-const Test = () => (
-  <div>test</div>
-);
-
 const steps = [
   {
     id: '1',
     message: 'Hello World',
-    trigger: '2',
-  },
-  {
-    id: '2',
-    options: [
-      { value: 'test', label: 'test', trigger: '3' },
-    ],
-  },
-  {
-    id: '3',
-    component: <Test />,
-    trigger: '22',
-  },
-  {
-    id: '22',
-    user: true,
-    trigger: '4',
-  },
-  {
-    id: '4',
-    options: [
-      { value: 'test', label: 'test', trigger: '3' },
-      { value: 'test2', label: 'end', trigger: '5' },
-    ],
-  },
-  {
-    id: '5',
-    message: 'Hi',
     end: true,
   },
 ];
 
 const ThemedExample = () => (
   <ThemeProvider theme={otherFontTheme}>
-    <ChatBot cache={true} floating={true} steps={steps} />
+    <ChatBot steps={steps} />
   </ThemeProvider>
 );
 
