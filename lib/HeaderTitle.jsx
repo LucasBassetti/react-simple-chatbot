37bd5315d2d9536200ef11f6f232e9423a6cc41f
Chris Chapman <chapman.chris.h@gmail.com>
2017-10-09 14:15:49 -0700

    Updating headertitle jsx to store font size

--- lib/HeaderTitle.jsx
+++ lib/HeaderTitle.jsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
+import defaultTheme from './theme';
 
 const HeaderTitle = styled.h2`
   margin: 0;
+  font-size:${({ theme }) => theme.headerFontSize};
 `;
+
+HeaderTitle.defaultProps = {
+  theme: defaultTheme,
+};
 
 export default HeaderTitle;
