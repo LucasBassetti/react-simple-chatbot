816ebb2f1948b9001834147e7eb1c5cc2351fbb1
Shy <puemos@gmail.com>
2017-11-11 12:47:30 +0100

    add mic events

--- lib/SubmitButton.jsx
+++ lib/SubmitButton.jsx
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
 
+const fillFunc = (props) => {
+  const { speaking, invalid } = props;
+
+  if (speaking) {
+    return 'blue';
+  }
+  return invalid ? '#E53935' : '#4a4a4a';
+};
+
 const SubmitButton = styled.button`
   background-color: transparent;
   border: 0;
   border-bottom-right-radius: 10px;
   box-shadow: none;
-  cursor: ${props => props.disabled ? 'default' : 'pointer'};
-  fill: ${props => props.invalid ? '#E53935' : '#4a4a4a'};
-  opacity: ${props => props.disabled && !props.invalid ? '.5' : '1'};
+  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
+  fill: ${fillFunc};
+  opacity: ${props => (props.disabled && !props.invalid ? '.5' : '1')};
   outline: none;
   padding: 14px 16px 12px 16px;
   position: absolute;
@@ -15,7 +24,7 @@ const SubmitButton = styled.button`
   top: 0;
 
   &:not(:disabled):hover {
-    opacity: .7;
+    opacity: 0.7;
   }
 `;
 
