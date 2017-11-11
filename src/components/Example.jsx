f78488a924d188d65b13c0a0e48c82c491661b67
Shy <puemos@gmail.com>
2017-11-11 16:27:53 +0100

    fix typo on readme

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -17,24 +17,15 @@ const otherFontTheme = {
 const steps = [
   {
     id: '1',
-    message: 'What is your name?',
-    trigger: '2',
-  },
-  {
-    id: '2',
-    user: true,
-    trigger: '3',
-  },
-  {
-    id: '3',
-    message: 'Hi {previousValue}, nice to meet you!',
+    message: 'Hello World',
     end: true,
   },
 ];
 
+
 const ThemedExample = () => (
   <ThemeProvider theme={otherFontTheme}>
-    <ChatBot steps={steps} recognitionEnable />
+    <ChatBot steps={steps} />
   </ThemeProvider>
 );
 
