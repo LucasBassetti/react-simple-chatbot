124024533eb4b6dbade0082b210459cdfe201c6d
Shy <puemos@gmail.com>
2017-11-11 15:21:48 +0100

    restore Example steps

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -17,17 +17,7 @@ const otherFontTheme = {
 const steps = [
   {
     id: '1',
-    message: 'What is your name?',
-    trigger: '2',
-  },
-  {
-    id: '2',
-    user: true,
-    trigger: '3',
-  },
-  {
-    id: '3',
-    message: 'Hi {previousValue}, nice to meet you!',
+    message: 'Hello World',
     end: true,
   },
 ];
