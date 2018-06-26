75eb28283a7df9cf40d22e8f458bb162cf156431
Malyita Alexandr <alex.malyita@gmail.com>
2018-06-26 11:35:39 +0300

    Image support, paste from clipboard and dropzone (#3)

---
+++ lib/steps/text/AttachmentPreview.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AttachmentPreview = styled.img`
+  box-shadow: rgba(0, 0, 0, 0.15) 0 1px 2px 0;
+  height: 40px;
+  min-width: 40px;
+  padding: 3px;
+  transform-origin: bottom right;
+  width: 40px;
+`;
+
+export default AttachmentPreview;
--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -14,18 +14,49 @@ const otherFontTheme = {
   userFontColor: '#4a4a4a',
 };
 
+const acceptedFileTypes = [
+  'image/png',
+  'image/jpeg',
+];
+
 const steps = [
   {
     id: '1',
-    message: 'Hello World',
-    end: true,
+    message: 'Hello, What\'s your name?',
+    trigger: '2',
   },
+  {
+    id: '2',
+    trigger: '3',
+    enableFiles: true,
+    validator: (value) => {
+      if (isNaN(value)) {
+        return 'value should be a number';
+      }
+      return true;
+    },
+    user: true
+  },
+  {
+    id: '3',
+    message: 'Nice to meet you, by',
+    end: true
+  }
 ];
 
+const handleEnd = ({ steps, values }) => {
+  // console.log(steps);
+  // console.log(values);
+  console.log(values);
+}
 
 const ThemedExample = () => (
   <ThemeProvider theme={otherFontTheme}>
-    <ChatBot steps={steps} />
+    <ChatBot
+      steps={steps}
+      acceptedFileTypes={acceptedFileTypes}
+      handleEnd={handleEnd}
+    />
   </ThemeProvider>
 );
 
