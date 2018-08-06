e5cc5b517e3636d5a497282cb8fee4abc25fefde
Fabian Gander <fabian.gander@garaio.com>
2018-08-06 14:36:16 +0200

    Adding BundleAnalyzer to show how the webpack bundled content looks like.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: path.resolve(__dirname, 'lib/index'),
@@ -20,6 +21,7 @@ module.exports = {
     new UglifyJsPlugin({
       comments: false,
     }),
+    new BundleAnalyzerPlugin(),
   ],
   module: {
     rules: [
