19a40306d68f5e31ee011b63b7fa2676e589ff91
Pascal Gagneur <pascal.gagneur@svt.se>
2019-04-03 08:01:02 +0200

    Shorthand return

--- lib/steps_components/options/OptionsStep.jsx
+++ lib/steps_components/options/OptionsStep.jsx
@@ -38,9 +38,7 @@ class OptionsStep extends Component {
     return (
       <OptionsStepContainer className="rsc-os">
         <Options className="rsc-os-options">
-          {Object.keys(options).map(key => {
-            return options[key];
-          }).map(this.renderOption)}
+          {Object.keys(options).map(key => options[key]).map(this.renderOption)}
         </Options>
       </OptionsStepContainer>
     );
