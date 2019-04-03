8cd37ca1c7da8f56bdc6101f3b5d45f018c39bdf
Pascal Gagneur <pascal.gagneur@svt.se>
2019-04-03 13:39:32 +0200

    Removed babel configuration from Webpack configs - Using babel.config.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,14 +34,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              '@babel/plugin-transform-arrow-functions',
-              '@babel/plugin-proposal-class-properties',
-            ],
-          },
+          loader: 'babel-loader'
         },
       },
     ],
