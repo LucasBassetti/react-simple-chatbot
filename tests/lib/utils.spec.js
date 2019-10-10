b06b796664ba3312e3ff4198e41c6e427922613a
Kapildev Neupane <kapildev@producement.com>
2019-10-10 14:24:06 +0545

    add tests for isNestedVariable

--- tests/lib/utils.spec.js
+++ tests/lib/utils.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
@@ -6,7 +7,19 @@ import { isNestedVariable, splitByFirstPeriod, insertIntoObjectByPath } from '..
 describe('Utils', () => {
   describe('isNestedVariable', () => {
     it('should work for a proper nested variable', () => {
-      expect(isNestedVariable('{variable.property1}')).to.be(true);
+      expect(isNestedVariable('{variable.property1}')).to.be.true;
     });
+
+    it('should not work for a proper non-nexted variable', () => {
+      expect(isNestedVariable('{variable}')).to.be.false;
+    });
+
+    it('should not work for a non-braced nested variable', () => {
+      expect(isNestedVariable('variable.property1')).to.be.false;
+    });
+
+    it('should not work for deeply nested variable', () => {
+      expect(isNestedVariable('{variable.prop1.prop2}')).to.be.true;
+    })
   });
 });
