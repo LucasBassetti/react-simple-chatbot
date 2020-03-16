307f7a1570c79127273b111f762cdadd56c71dbc
Kapildev Neupane <kapildev@producement.com>
2020-03-16 17:46:15 +0545

    Fix loading UX: seamless transition from loading to text message

--- lib/ChatBot.jsx
+++ lib/ChatBot.jsx
@@ -599,7 +599,9 @@ class ChatBot extends Component {
 
       nextStep = nextSteps[lastIndex];
 
-      if (nextStep.message) {
+      const nextStepIsTextStep = nextStep.message;
+      if (nextStepIsTextStep) {
+        nextStep.animated = false;
         nextStep.message = this.getStepMessage(nextStep.message);
       }
     }
