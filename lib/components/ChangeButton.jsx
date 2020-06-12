8012d613c61f858c25436764028c3ac7f585d32f
Haya4Taka <mumon.mum@gmail.com>
2020-06-12 18:17:18 +0900

    表示変更ボタンの作成

---
+++ lib/components/ChangeButton.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import defaultTheme from '../theme';
+
+const ChangeButton = styled.button`
+  background-color #EEE;
+  border: none;
+  border-radius: 10px;
+  box-sizing: border-box;
+  cursor: pointer;
+  height: 26px;
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  min-width: 60px;
+  transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+ChangeButton.defaultProps = {
+  theme: defaultTheme
+};
+
+export default ChangeButton;
