ae32cf4251b5fcbd15ecfa6f7bf08003910fc107
Haya4Taka <mumon.mum@gmail.com>
2020-06-14 14:55:01 +0900

    オプションエレメントの見た目調整

--- lib/components/InputOptionElement.jsx
+++ lib/components/InputOptionElement.jsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
 const InputOptionElement = styled.button`
+  background-color: #ddd;
+  border: 0;
+  border-radius: 10px;
+  cursor: pointer;
   height: 40px;
   width: 60px;
+
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 export default InputOptionElement;
