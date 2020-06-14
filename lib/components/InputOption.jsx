150ef7457b42dc93425327708d894d53209fbb32
Haya4Taka <mumon.mum@gmail.com>
2020-06-14 14:46:37 +0900

    Flexによる位置調整に変更

--- lib/components/InputOption.jsx
+++ lib/components/InputOption.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const InputOption = styled.div`
   background-color: transparent;
+  display: flex;
+  align-items: center;
   height: 50px;
   margin: 0 0 0 70px;
   position: relative;
--- lib/components/InputOptionElement.jsx
+++ lib/components/InputOptionElement.jsx
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 const InputOptionElement = styled.button`
   height: 40px;
   width: 60px;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
 `;
 
 export default InputOptionElement;
