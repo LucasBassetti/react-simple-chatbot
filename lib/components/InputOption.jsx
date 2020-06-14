2cc0396e2de572708316c6fb46d3435a7a9a3b91
Haya4Taka <mumon.mum@gmail.com>
2020-06-14 14:13:26 +0900

    選択肢の箱を作成

---
+++ lib/components/InputOption.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const InputOption = styled.div`
+  background-color: trasparent;
+  height: 50px;
+  margin: 0 0 0 70px;
+  width: 100%;
+`;
+
+export default InputOption;
